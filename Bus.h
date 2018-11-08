7bb70de9080975d9ba7baeb41116e4c79c6d5d0d
nat1994 <nathanaelchan94@gmail.com>
2018-11-08 16:09:58 +0800

    added the function to run the bus

--- Bus.h
+++ Bus.h
@@ -4,6 +4,7 @@
 #include"pipe.h"
 #include"Memory.h"
 #include"Cache.h"
+#include<"message.h">
 struct bus{
     struct pipe *pipe_to_C0;
     struct pipe *pipe_from_C0;
@@ -31,6 +32,77 @@ struct bus * bus_init(struct L1_cache *C0, struct L1_cache *C1, struct L1_cache 
     bus->pipe_to_mem = mem->pipe_from_bus;
 }
 
-void bus_run(struct bus *bus, long int cycle);
+void forward_message(msg *message){
+    switch(message->dest){
+        case BROADCAST:
+            write_pipe(pipe_to_C0,message);
+            write_pipe(pipe_to_C1,message);
+            write_pipe(pipe_to_C2,message);
+            write_pipe(pipe_to_C3,message);
+            write_pipe(pipe_to_mem,message);
+            break;
+        case CACHE0_ID:
+            write_pipe(pipe_to_C0,message);
+            break;
+        case CACHE1_ID:
+            write_pipe(pipe_to_C1,message);
+            break;
+        case CACHE2_ID:
+            write_pipe(pipe_to_C2,message);
+            break;
+        case CACHE3_ID:
+            write_pipe(pipe_to_C3,message);
+            break;
+        case MEMORY_ID:
+            write_pipe(pipe_to_mem,message);
+            break;
+    }
+}
+
+/*
+- Takes the message (from processor and memeory)and sends it to the pipe of the destination. 
+- Processor has more priority than memory. 
+- Each cycle the bus only processes 1 message. Can use read_pipe and write_pipe if needed.  
+*/
+
+void bus_run(struct bus *bus, long int cycle){
+    msg *from_C0; //Message from C0
+    msg *from_C1; //Message from C1
+    msg *from_C2; //Message from C2
+    msg *from_C3; //Message from C3
+    msg *from_mem; //Message from mem
+
+    msg *message;
+
+
+    /* Reading in all the messages from the pipe*/ 
+    from_C0 = peek_at_msg(bus->pipe_from_C0);
+    from_C1 = peek_at_msg(bus->pipe_from_C1);
+    from_C2 = peek_at_msg(bus->pipe_from_C2);
+    from_C3 = peek_at_msg(bus->pipe_from_C3);
+    from_mem = peek_at_msg(bus->pipe_from_mem);
+
+    if(from_C0->cycle == cycle){
+        message = read_pipe(bus->pipe_from_C0);
+        forward_msg(message);
+    }
+    else if(from_C1->cycle == cycle){
+        message = read_pipe(bus->pipe_from_C1);
+        forward_msg(message);
+    }
+    else if(from_C2->cycle == cycle){
+        message = read_pipe(bus->pipe_from_C2);
+        forward_msg(message);
+    }
+    else if(from_C3->cycle == cycle){
+        message = read_pipe(bus->pipe_from_C3);
+        forward_msg(message);
+    }
+    else if(from_mem->cycle == cycle){
+        message = read_pipe(bus->pipe_from_mem);
+        forward_msg(message);
+    }
+};
+
 #endif // BUS
 
--- Cache.h
+++ Cache.h
@@ -19,18 +19,34 @@ struct cache_block{
     unsigned int shared_line;
 };
 struct cache_bank{
-    struct cache_block *blocks;
+    struct cache_block *blocks; //array of cache blocks. Need to create based on input. 
 };
 struct L1_cache{
-    struct pipe *pipe_to_pro;
+    struct pipe *pipe_to_pro; //L1_cache to processor 
     struct pipe *pipe_from_pro;
     struct pipe *pipe_to_bus;
     struct pipe *pipe_from_bus;
     struct cache_bank *banks;
     unsigned int set_index_mask;
+    int block_size;
 };
 
-void cache_init(int cache_size, int associativity, int block_size, char* protocol);
-void cache_run(struct L1_cache *cache, long int cycle);
-int lookup_cache(struct L1_cache *cache, unsigned int addr);
+void cache_init(int cache_size, int associativity, int block_size, char* protocol); //Create the L1_cache
+void cache_run(struct L1_cache *cache, long int cycle); 
+
+/*
+This function will check the processor for a message. 
+Depending on whether its a cache hit or miss. 
+For a cache hit, it will send a message back to the processor to say that it has the data
+If cache miss, then it will send a message to the bus to say that it doesn't have the data. 
+If there is no message from processor, the cache will check if there are any messages from bus. Bus message may be bus update, or request from other cache.
+
+If bus update, then we need to update the status of the cache block. (write in Dragon.h)
+
+if more than one cache has the data, the first cache will respond while the rest will not. 
+
+In 1 cycle, the cache can only send or read a message. Cannot do both. 
+*/
+
+int lookup_cache(struct L1_cache *cache, unsigned int addr); //To check if the address is in the cache block
 #endif // CACHE
