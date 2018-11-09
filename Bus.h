c40ec5ddf439656d3b1cd398c54d20ba58998b71
nat1994 <nathanaelchan94@gmail.com>
2018-11-09 12:03:20 +0800

    resolved some errors in bus

--- Bus.h
+++ Bus.h
@@ -18,7 +18,7 @@ struct bus{
     struct pipe *pipe_from_mem;
 };
 
-struct void bus_init(struct L1_cache *C0, struct L1_cache *C1, struct L1_cache *C2, struct L1_cache *C3, struct memory *mem){
+void bus_init(struct L1_cache *C0, struct L1_cache *C1, struct L1_cache *C2, struct L1_cache *C3, struct memory *mem){
     struct bus *bus = malloc(sizeof(struct bus));
     bus->pipe_from_C0 = C0->pipe_to_bus;
     bus->pipe_to_C0 = C0->pipe_from_bus;
@@ -32,29 +32,29 @@ struct void bus_init(struct L1_cache *C0, struct L1_cache *C1, struct L1_cache *
     bus->pipe_to_mem = mem->pipe_from_bus;
 }
 
-void forward_message(msg *message){
+void forward_msg(struct bus *bus, struct msg *message){
     switch(message->dest){
         case BROADCAST:
-            write_pipe(pipe_to_C0,message);
-            write_pipe(pipe_to_C1,message);
-            write_pipe(pipe_to_C2,message);
-            write_pipe(pipe_to_C3,message);
-            write_pipe(pipe_to_mem,message);
+            write_pipe(bus->pipe_to_C0,message);
+            write_pipe(bus->pipe_to_C1,message);
+            write_pipe(bus->pipe_to_C2,message);
+            write_pipe(bus->pipe_to_C3,message);
+            write_pipe(bus->pipe_to_mem,message);
             break;
         case CACHE0_ID:
-            write_pipe(pipe_to_C0,message);
+            write_pipe(bus->pipe_to_C0,message);
             break;
         case CACHE1_ID:
-            write_pipe(pipe_to_C1,message);
+            write_pipe(bus->pipe_to_C1,message);
             break;
         case CACHE2_ID:
-            write_pipe(pipe_to_C2,message);
+            write_pipe(bus->pipe_to_C2,message);
             break;
         case CACHE3_ID:
-            write_pipe(pipe_to_C3,message);
+            write_pipe(bus->pipe_to_C3,message);
             break;
         case MEMORY_ID:
-            write_pipe(pipe_to_mem,message);
+            write_pipe(bus->pipe_to_mem,message);
             break;
     }
 }
@@ -66,11 +66,11 @@ void forward_message(msg *message){
 */
 
 void bus_run(struct bus *bus, long int cycle){
-    msg *from_C0; //Message from C0
-    msg *from_C1; //Message from C1
-    msg *from_C2; //Message from C2
-    msg *from_C3; //Message from C3
-    msg *from_mem; //Message from mem
+    struct msg *from_C0 = malloc(sizeof(struct msg)); //Message from C0
+    struct msg *from_C1 = malloc(sizeof(struct msg)); //Message from C1
+    struct msg *from_C2 = malloc(sizeof(struct msg)); //Message from C2
+    struct msg *from_C3 = malloc(sizeof(struct msg)); //Message from C3
+    struct msg *from_mem = malloc(sizeof(struct msg)); //Message from mem
 
     msg *message;
 
@@ -84,23 +84,23 @@ void bus_run(struct bus *bus, long int cycle){
 
     if(from_C0->cycle == cycle){
         message = read_pipe(bus->pipe_from_C0);
-        forward_msg(message);
+        forward_msg(bus, message);
     }
     else if(from_C1->cycle == cycle){
         message = read_pipe(bus->pipe_from_C1);
-        forward_msg(message);
+        forward_msg(bus, message);
     }
     else if(from_C2->cycle == cycle){
         message = read_pipe(bus->pipe_from_C2);
-        forward_msg(message);
+        forward_msg(bus, message);
     }
     else if(from_C3->cycle == cycle){
         message = read_pipe(bus->pipe_from_C3);
-        forward_msg(message);
+        forward_msg(bus, message);
     }
     else if(from_mem->cycle == cycle){
         message = read_pipe(bus->pipe_from_mem);
-        forward_msg(message);
+        forward_msg(bus, message);
     }
 };
 
